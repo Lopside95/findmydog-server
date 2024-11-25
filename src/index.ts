eaf6eaf616c736cc6cdef1e2e0c478b0358fa1e1
James Wallington <jameswallington11@gmail.com>
2024-11-25 20:12:46 +0000

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express, { Express, Request, Response } from "express";
 import "dotenv/config";
 import posts from "./routes/posts.ts";
 import users from "./routes/users.ts";
+import cors from "cors";
 
 const app: Express = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/", posts);
 app.use("/users", users);
