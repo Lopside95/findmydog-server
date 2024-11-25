c6a1ad0c29dc5d05af5f22498cdb9cf42ef77b3a
James Wallington <jameswallington11@gmail.com>
2024-11-25 16:20:48 +0000

    add posts table migration

--- knexfile.ts
+++ knexfile.ts
@@ -1,14 +1,15 @@
-import Knex from "knex";
 import "dotenv/config";
 
-const knex = Knex({
+export default {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    charset: "utf8",
   },
-});
-
-export default knex;
+  //   migrations: {
+  //     extension: "ts",
+  //   },
+};
