9a9c8cd9b9924b7c809902039f27379d6d9a80d3
James Wallington <jameswallington11@gmail.com>
2024-12-03 19:50:53 +0000

    removed firebase

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -12,7 +12,7 @@ export type Post = {
   title: string;
   img?: string;
   description: string;
-  urgency: number;
+  urgency: number; // use colours / etc. in frontend
   type: PostType;
   status: PostStatus;
   tags: Tag[];
@@ -43,60 +43,3 @@ export enum PostStatus {
   "OPEN",
   "CLOSED",
 }
-
-// import z from "zod";
-
-// export const tag = z.object({
-//   id: z.string(),
-//   name: z.string(),
-//   // active: z.boolean(),
-// });
-
-// export const postSchema = z.object({
-//   // id: z.string(),
-//   title: z.string(),
-//   img: z.string().optional(),
-//   description: z.string(),
-//   urgency: z.number(),
-//   type: z.enum(["General", "Report"]),
-//   status: z.enum(["Open", "Closed"]),
-//   tags: z.array(tag),
-//   longitude: z.number().optional(),
-//   latitude: z.number().optional(),
-//   // created_at: z.date(),
-//   // updated_at: z.date(),
-// });
-
-// export const userSchema = z.object({
-//   id: z.string(),
-//   firstName: z.string().min(1),
-//   lastName: z.string(),
-//   email: z.string().email(),
-//   password: z.string(),
-//   active: z.boolean(),
-//   posts: z.array(postSchema),
-//   created_at: z.date(),
-//   updated_at: z.date(),
-// });
-
-// // export const commentSchema = z.object({
-
-// // });
-
-// export type User = z.infer<typeof userSchema>;
-// export type Tag = z.infer<typeof tag>;
-// export type Post = z.infer<typeof postSchema>;
-
-// export type Post = {
-//   id: number;
-//   title: string;
-//   longitude: number;
-//   latitude: number;
-//   img: string;
-//   description: string;
-//   urgency: number;
-//   type: PostType;
-//   status: PostStatus;
-//   created_at: Date;
-//   updated_at: Date;
-// };
